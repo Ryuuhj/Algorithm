99dd4908dadcc512898100a50219a0cf35e1ee5c
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2022-07-19 00:09:18 +0900

    2309번
    bronze 2

---
+++ bronze/일곱 난쟁이.py
@@ -0,0 +1,13 @@
+import random
+height = []
+s =[]
+for i in range(9):
+    height.append(int(input()))
+while True:
+    real = random.sample(height,7)
+    if sum(real) == 100:
+      s = sorted(real)
+      break
+
+for k in s:
+  print(k)
