e394bf9ae1dd4ece17ab6ea1090ee28b4219b45f
Ryuuhj <97296543+Ryuuhj@users.noreply.github.com>
2022-08-25 22:48:58 +0900

    2156번

--- DP/포도주 시식.py
+++ DP/포도주 시식.py
@@ -7,7 +7,7 @@ for i in range(cnt):
 dp= [0]*10000
 dp[0]=w[0]
 dp[1]=w[0]+w[1]
-dp[2]=max(dp[0]+w[2], w[2]+w[1], dp[1])
+dp[2]=max(dp[1],max(w[0],w[1])+w[2])
 for i in range(3,cnt):
     dp[i]=max(dp[i-2]+w[i], dp[i-3]+w[i-1]+w[i], dp[i-1])
 print(max(dp))
